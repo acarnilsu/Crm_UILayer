2931a6177d027e6371ebe15e37d73000a665ab51
nilsu <nilsu@DESKTOP-O6Q5UAT>
2022-09-27 23:41:27 +0300

    update

---
+++ Crm_UILayer/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crm_UILayer.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+        public IActionResult Index()
+        {
+            var values = employeeManager.TGetListAll        ();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            employeeManager.TInsert(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
