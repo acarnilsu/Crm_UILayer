b94b398ee6484a3e4741e8f1758b2e012c5b98e1
acarnilsu <nilsuacar42@gmail.com>
2022-10-20 22:25:11 +0300

    UPDATE

---
+++ Crm_UILayer/Controllers/MessageController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Crm_UILayer.Controllers
+{
+    public class MessageController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMessageService _messageService;
+
+        public MessageController(UserManager<AppUser> userManager, IMessageService messageService)
+        {
+            _userManager = userManager;
+            _messageService = messageService;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            var mail = await _userManager.FindByEmailAsync(User.Identity.Name);
+            ViewBag.v = mail;
+            var values = _messageService.TGetReceiverMessageList(mail.Email);
+            return View();
+        }
+        public IActionResult Outbox()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message p)
+        {
+            return View();
+        }
+
+        public IActionResult MessageDetails(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EfMessageDal.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfMessageDal : GenericRepository<Message>, IMessageDal
+    {
+        Context context = new Context();
+        public List<Message> GetReceiverMessageList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
