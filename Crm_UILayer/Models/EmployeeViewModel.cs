5ddcd364dda5923554295c85b27df0d09d305f80
acarnilsu <nilsuacar42@gmail.com>
2022-11-10 21:04:42 +0300

    UPDATE

---
+++ Crm_UILayer/Models/EmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Crm_UILayer.Models
+{
+    public class EmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmployeeMail { get; set; }
+        public string EmployeeGender { get; set; }
+
+    }
+}
